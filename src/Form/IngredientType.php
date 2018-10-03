cf9be5a4581575e07b30e519a2f4bb224d7b9155
Ingrid <ingrid.weil@coding-academy.fr>
2018-10-03 14:59:31 +0200

    formulaire ajout ingredient

---
+++ src/Form/IngredientType.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class IngredientType extends AbstractType
+{
+    /**
+     * Formulaire concernant l'objet Ingredient
+     * {@inheritDoc}
+     * @see \Symfony\Component\Form\AbstractType::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('denomination')
+            ->add('quantite')
+            ->add('image');
+
+        $builder->add('save', SubmitType::class, array(
+            'label' => $options['label_submit'],
+            'attr' => array(
+                'class' => 'btn btn-primary'
+            )
+        ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+            'label_submit' => 'Valider'
+        ]);
+    }
+}
