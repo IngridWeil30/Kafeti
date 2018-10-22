28ed1e987f3adcc2ab1a8e817ec4b59765aafb7d
IngridWeil30 <37538927+IngridWeil30@users.noreply.github.com>
2018-10-22 11:06:57 +0200

    Fonctionalités Ingredient
    see/edit/delete

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Form\IngredientType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class IngredientController extends AppController
 {
@@ -104,5 +105,82 @@ class IngredientController extends AppController
                 'active' => "Ajout d'un ingrédient"
             )
         ));
+    }
+    
+    /**
+     * Fiche d'un ingrédient
+     *
+     * @Route("/ingredient/see/{id}", name="ingredient_see")
+     * @ParamConverter("ingredient", options={"mapping": {"id": "id"}})
+     */
+    public function seeAction(Ingredient $ingredient)
+    {
+        return $this->render('ingredient/see.html.twig', array(
+            'ingredient' => $ingredient,
+            'paths' => array(
+                'home' => $this->indexUrlProject(),
+                'urls' => [
+                    $this->generateUrl('ingredient_listing') => "Listing des ingrédients"
+                ],
+                'active' => "Fiche d'un ingrédient"
+            )
+        ));
+    }
+    
+    /**
+     * Edition d'un ingrédient
+     *
+     * @Route("/ingredient/edit/{id}", name="ingredient_edit")
+     * @ParamConverter("ingredient", options={"mapping": {"id": "id"}})
+     */
+    public function editAction(Request $request, Ingredient $ingredient)
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $em = $this->getDoctrine()->getManager();
+            
+            $em->persist($ingredient);
+            $em->flush();
+        }
+        
+        return $this->render('ingredient/edit.html.twig', array(
+            'ingredient' => $ingredient,
+            'form' => $form->createView(),
+            'paths' => array(
+                'home' => $this->indexUrlProject(),
+                'urls' => [
+                    $this->generateUrl('ingredient_listing') => "Listing des ingrédients"
+                ],
+                'active' => "Edition d'un ingrédient"
+            )
+        ));
+    }
+    
+    /**
+     * Désactivation d'un ingrédient
+     *
+     * @Route("/ingredient/delete/{id}", name="ingredient_delete")
+     * @ParamConverter("ingredient", options={"mapping": {"id": "id"}})
+     * @Security("is_granted('ROLE_GERANT')")
+     *
+     */
+    public function deleteAction(Request $request, Ingredient $ingredient)
+    {
+        if ($ingredient->getActif() == 0) {
+            $ingredient->setActif(1);
+        } else {
+            $ingredient->setActif(0);
+        }
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $entityManager->persist($ingredient);
+        
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('ingredient_listing');
     }
 }
--- src/DataFixtures/LoadIngredientData.php
+++ src/DataFixtures/LoadIngredientData.php
@@ -16,42 +16,48 @@ class LoadIngredientData extends Fixture implements DependentFixtureInterface
             'setQuantite' => 22.5,
             'setSeuilAlerte' => 10,
             'setActif' => 1,
-            'setTypeIngredient' => 'TypeIngredient-1'
+            'setTypeIngredient' => 'TypeIngredient-1',
+            'setImage' => 'https://cdn1.fermedesaintemarthe.com/I-Autre-21920_1200x1200-carotte-de-colmar-a-coeur-rouge-2-ab.net.jpg'
             ],
             'Ingredient-2' => [
                 'setDenomination' => 'Boeuf',
                 'setQuantite' => 12,
                 'setSeuilAlerte' => 10,
                 'setActif' => 1,
-                'setTypeIngredient' => 'TypeIngredient-2'
+                'setTypeIngredient' => 'TypeIngredient-2',
+                'setImage' => 'https://static.cuisineaz.com/610x610/i130833-roti-de-boeuf-au-cookeo.jpeg'
             ],
             'Ingredient-3' => [
                 'setDenomination' => 'Cabillaud',
                 'setQuantite' => 13,
                 'setSeuilAlerte' => 10,
                 'setActif' => 1,
-                'setTypeIngredient' => 'TypeIngredient-3'
+                'setTypeIngredient' => 'TypeIngredient-3',
+                'setImage' => 'http://res.cloudinary.com/hv9ssmzrz/image/fetch/c_fill,f_auto,h_288,q_auto,w_512/http://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/recipe_photos/slide/325/cabillaud-vapeur-0.jpg'
             ],
             'Ingredient-4' => [
                 'setDenomination' => 'Pomme de terre',
                 'setQuantite' => 15.5,
                 'setSeuilAlerte' => 10,
                 'setActif' => 1,
-                'setTypeIngredient' => 'TypeIngredient-4'
+                'setTypeIngredient' => 'TypeIngredient-4',
+                'setImage' => 'https://www.meillandrichardier.com/media/page/resized/page/images/4/8/1000x0/resized-4826-pdt-amandine-germicopa-0002-taille1000-ho.jpg'
             ],
             'Ingredient-5' => [
                 'setDenomination' => 'Kiwi',
                 'setQuantite' => 30,
                 'setSeuilAlerte' => 10,
                 'setActif' => 1,
-                'setTypeIngredient' => 'TypeIngredient-5'
+                'setTypeIngredient' => 'TypeIngredient-5',
+                'setImage' => 'https://www.lanutrition.fr/sites/default/files/styles/article_large/public/ressources/kiwis_bol_4.jpg?itok=spCQy2KN'
             ],
             'Ingredient-6' => [
                 'setDenomination' => 'Confiture de mandarine',
                 'setQuantite' => 5,
                 'setSeuilAlerte' => 5,
                 'setActif' => 1,
-                'setTypeIngredient' => 'TypeIngredient-6'
+                'setTypeIngredient' => 'TypeIngredient-6',
+                'setImage' => 'https://static.cuisineaz.com/400x320/i120384-confiture-de-mandarines.jpeg'
             ]
         ]
     ];
